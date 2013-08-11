68b6735a091f31168cc68c03af50ac88368ef086
Greg Lazarev <greg@thoughtbot.com>
2013-08-10 18:18:39 -0700

    Extract looking up user by token into method

--- lib/clearance/session.rb
+++ lib/clearance/session.rb
@@ -20,8 +20,7 @@ module Clearance
 
     def current_user
       if remember_token.present?
-        @current_user ||= Clearance.configuration.user_model.
-          where(remember_token: remember_token).first
+        @current_user ||= user_from_remember_token(remember_token)
       end
 
       @current_user
@@ -57,6 +56,10 @@ module Clearance
 
     def remember_token
       cookies[REMEMBER_TOKEN_COOKIE]
+    end
+
+    def user_from_remember_token(token)
+      Clearance.configuration.user_model.where(remember_token: token).first
     end
   end
 end
